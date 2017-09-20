f35587d781d1042b7d56e2cebc37e3c9d8395e37
nienkebos <nienkebos@live.com>
2017-09-20 14:58:42 +0200

    push data to firebasedb

--- src/App.js
+++ src/App.js
@@ -7,20 +7,38 @@ class App extends Component {
     //super:overwriting constructor, but still want the normal constructor things to happen...
     super(props);
     this.state = {
-      data: null
+      data: null,
+      newData: '',
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
   componentDidMount() {
     //database.ref: create connection to db.
     //snapshot: one moment; abstracts over the data.
-    // snapshot.val gets the data of that point in the tree
+    //snapshot.val gets the data of that point in the tree
     database.ref().on('value', (snapshot) => {
       this.setState({
         data: snapshot.val()
       })
-      // console.log('THE DATA CHANGED!', snapshot.val());
+      //console.log('THE DATA CHANGED!', snapshot.val());
     });
   }
+
+  handleChange(event) {
+    const newData = event.target.value;
+    this.setState({
+      newData
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    database.ref('/New Data')
+            .push(this.state.newData);
+  }
+
   render() {
     return (
       <div className="App">
@@ -31,8 +49,12 @@ class App extends Component {
           Rebuilding my app in react... let's see what happens!
         </p>
         <pre className="App-data">
-          { JSON.stringify(this.state.data, null, 2) }
+          {/* { JSON.stringify(this.state.data, null, 2) } */}
         </pre>
+        <form className="App-form" onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.newData} onChange={this.handleChange}/>
+          <input type="submit" />
+        </form>
       </div>
     );
   }
